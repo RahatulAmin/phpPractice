2ff64ecabe00b52fd9d9df15f06265f741e45caa
ananto <rahatul.amin.ananto@gmail.com>
2018-02-11 17:15:27 +0600

    upload complete

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Upload File to Mysql database practice	
+	</title>
+</head>
+<body>
+	<form action="uploadpractice.php" method="post" enctype="multipart/form-data" >
+		<input type="file" name="file">
+		<button type="submit" name="submit">Upload</button>
+	</form>
+</body>
+</html>
