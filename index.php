8e5a9bcaffb5af9752c7e793f5d28ab0ceb3277e
ananto <rahatul.amin.ananto@gmail.com>
2018-02-11 17:49:47 +0600

    upload complete

--- index.php
+++ index.php
@@ -7,8 +7,32 @@
 </head>
 <body>
 	<form action="uploadpractice.php" method="post" enctype="multipart/form-data" >
+		<br/>
 		<input type="file" name="file">
+		<br/>
+		<br/>
+		<input type="text" name="description" placeholder="Please Describe the image">
+		<br/>
+		<br/>
 		<button type="submit" name="submit">Upload</button>
+		<br/>
+		<br/>
 	</form>
 </body>
 </html>
+
+<?php
+	$dbc = mysqli_connect('localhost','root','','upload_practice');
+	$r = array();
+	$query = "SELECT * FROM upload";
+	$result = mysqli_query($dbc,$query);
+	while($row = mysqli_fetch_array($result)){
+		echo $row[1];
+		?>
+		<br/> 
+		<img src="uploads/<?php echo $row[3] ?>"> <br/> <br/>  <?php
+	}
+	
+	
+	mysqli_close($dbc);
+?>
--- uploadpractice.php
+++ uploadpractice.php
@@ -8,17 +8,21 @@
 		$fileError = $_FILES['file']['error'];
 		$fileType = $_FILES['file']['type'];
 
-		// $fileExt = explode('.', '$fileName');
-		// $fileActualExt = strtolower(end($fileExt));
+		$description = $_POST['description'];
 
 		$allowed = array('jpg','jpeg','png','pdf');
 
-		 // if (in_array($fileActualExt, $allowed)) {
 			if ($fileError === 0) {
 				if ($fileSize < 500000) {
 					
 					$fileDestination = 'uploads/'.$fileName;
 					move_uploaded_file($filetmp, $fileDestination);
+
+					$dbc = mysqli_connect('localhost','root','','upload_practice');
+					$query = "INSERT INTO `upload`(`description`, `picture`) VALUES ('$description','$fileName')";
+					mysqli_query($dbc,$query);
+					mysqli_close($dbc);
+
 					header("Location: index.php?uploadSuccess");
 				}else{
 					echo "Your file is too big";
@@ -26,8 +30,6 @@
 			}else{
 				echo "There is an error uploading the file";
 			}
-		  // }else{
-		  // 	echo "File type not supported";
-		  // }
+		
 	}
 ?>
